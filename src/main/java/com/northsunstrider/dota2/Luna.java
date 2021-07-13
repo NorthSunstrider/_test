a4f5fd32abc682a1ea0c195bd6c99be979e80b3c
NorthSunstrider <north.tong@163.com>
2021-07-13 12:05:20 +0800

    add dota2 luna test

---
+++ src/main/java/com/northsunstrider/dota2/Luna.java
@@ -0,0 +1,60 @@
+package com.northsunstrider.dota2;
+
+/**
+ * @author North
+ * @date 2021/05/09
+ */
+public class Luna {
+
+    /**
+     * @param args
+     */
+    public static void main(String[] args) {
+        moonGlaivesOldVersion();
+
+        moonGlaivesVersion728();
+
+    }
+
+    public static void moonGlaivesOldVersion() {
+        int i[] = {1, 2, 3, 6};
+        int attack = 100;
+        int totalDamage;
+        for (int x = 0; x < i.length; x++) {
+            System.out.print(attack);
+            double temp = attack;
+            totalDamage = attack;
+            for (int y = 0; y < i[x]; y++) {
+                temp = temp * 0.65;
+                totalDamage += temp;
+                System.out.print("\t" + temp);
+
+            }
+            System.out.println();
+            System.out.println(totalDamage);
+        }
+
+    }
+
+    public static void moonGlaivesVersion728() {
+        int i[] = {3, 4, 5, 6};
+        double j[] = {0.5, 0.56, 0.62, 0.68};
+        int attack = 100;
+        int totalDamage;
+        for (int x = 0; x < i.length; x++) {
+            System.out.print(attack);
+            double temp = attack;
+            totalDamage = attack;
+            for (int y = 0; y < i[x]; y++) {
+                temp = temp * j[x];
+                totalDamage += temp;
+                System.out.print("\t" + temp);
+
+            }
+            System.out.println();
+            System.out.println(totalDamage);
+        }
+
+    }
+
+}
