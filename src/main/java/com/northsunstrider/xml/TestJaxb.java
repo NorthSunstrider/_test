95f72b52976ca35d76b790851825feb3dee2be27
NorthSunstrider <north.tong@163.com>
2021-12-05 15:32:05 +0800

    Update TestJaxb.java

--- src/main/java/com/northsunstrider/xml/TestJaxb.java
+++ src/main/java/com/northsunstrider/xml/TestJaxb.java
@@ -1,43 +1,47 @@
 package com.northsunstrider.xml;
 
-
 import java.io.StringReader;
 
 import javax.xml.bind.JAXBContext;
 import javax.xml.bind.JAXBException;
 import javax.xml.bind.Marshaller;
 import javax.xml.bind.Unmarshaller;
+
 import org.junit.Test;
 
+/*
+ * jdk不在内置javax，需要在maven中额外引入 这样就麻烦了，不如直接使用dom4j这个类库
+ */
 public class TestJaxb {
 
-	@Test
-	public void beanToXML() {
-		Classroom classroom = new Classroom(1, "软件工程", 4);
-		Student student = new Student(101, "张三", 22, classroom);
+    @Test
+    public void beanToXML() {
+        Classroom classroom = new Classroom(1, "软件工程", 4);
+        Student student = new Student(101, "张三", 22, classroom);
 
-		try {
-			JAXBContext context = JAXBContext.newInstance(Student.class);
-			Marshaller marshaller = context.createMarshaller();
-			marshaller.marshal(student, System.out);
-		} catch (JAXBException e) {
-			e.printStackTrace();
-		}
+        try {
+            JAXBContext context = JAXBContext.newInstance(Student.class);
+            Marshaller marshaller = context.createMarshaller();
+            marshaller.marshal(student, System.out);
+        } catch (JAXBException e) {
+            e.printStackTrace();
+        }
 
-	}
-	
-	@Test
-	public void XMLStringToBean(){
-		String xmlStr = "<?xml version=\"1.0\" encoding=\"UTF-8\" standalone=\"yes\"?><student><age>22</age><classroom><grade>4</grade><id>1</id><name>软件工程</name></classroom><id>101</id><name>张三</name></student>";
-		try {
-			JAXBContext context = JAXBContext.newInstance(Student.class);
-			Unmarshaller unmarshaller = context.createUnmarshaller();
-			Student student = (Student)unmarshaller.unmarshal(new StringReader(xmlStr));
-			System.out.println(student.getAge());
-			System.out.println(student.getClassroom().getName());
-		} catch (JAXBException e) {
-			e.printStackTrace();
-		}
-		
-	}
+    }
+
+    @Test
+    public void XMLStringToBean() {
+        String xmlStr =
+            "<?xml version=\"1.0\" encoding=\"UTF-8\" standalone=\"yes\"?><student><age>22</age><classroom><grade>4</grade><id>1</id><name>软件工程</name></classroom><id>101</id><name>张三</name></student>";
+        try {
+            JAXBContext context = JAXBContext.newInstance(Student.class);
+            Unmarshaller unmarshaller = context.createUnmarshaller();
+            Student student = (Student)unmarshaller.unmarshal(new StringReader(xmlStr));
+            System.out.println(student.getAge());
+            System.out.println(student.getClassroom().getName());
+        } catch (JAXBException e) {
+            e.printStackTrace();
+        }
+
+    }
 }
