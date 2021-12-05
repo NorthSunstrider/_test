f148c94859c94905678480f2e866b8996453398b
NorthSunstrider <north.tong@163.com>
2021-12-05 15:30:15 +0800

    Create Consumer.java

---
+++ src/main/java/com/northsunstrider/rocketmq/Consumer.java
@@ -0,0 +1,40 @@
+package com.northsunstrider.rocketmq;
+
+/**
+ * @author North
+ * @date 2021/11/18
+ */
+import java.util.List;
+
+import org.apache.rocketmq.client.consumer.DefaultMQPushConsumer;
+import org.apache.rocketmq.client.consumer.listener.ConsumeConcurrentlyContext;
+import org.apache.rocketmq.client.consumer.listener.ConsumeConcurrentlyStatus;
+import org.apache.rocketmq.client.consumer.listener.MessageListenerConcurrently;
+import org.apache.rocketmq.client.exception.MQClientException;
+import org.apache.rocketmq.common.message.MessageExt;
+
+public class Consumer {
+
+    public static void main(String[] args) throws InterruptedException, MQClientException {
+
+        // 实例化消费者
+        DefaultMQPushConsumer consumer = new DefaultMQPushConsumer("zhuzeqing-1");
+
+        // 设置NameServer的地址
+        consumer.setNamesrvAddr("localhost:9876");
+
+        // 订阅一个或者多个Topic，以及Tag来过滤需要消费的消息
+        consumer.subscribe("zhuzeqing-1-Topic", "*");
+        // 注册回调实现类来处理从broker拉取回来的消息
+        consumer.registerMessageListener(new MessageListenerConcurrently() {
+            public ConsumeConcurrentlyStatus consumeMessage(List<MessageExt> msgs, ConsumeConcurrentlyContext context) {
+                System.out.printf("%s Receive New Messages: %s %n", Thread.currentThread().getName(), msgs);
+                // 标记该消息已经被成功消费
+                return ConsumeConcurrentlyStatus.CONSUME_SUCCESS;
+            }
+        });
+        // 启动消费者实例
+        consumer.start();
+        System.out.printf("Consumer Started.%n");
+    }
+}
