9dc2a9d6ad252489efac784c4f957ae99e23cb36
NorthSunstrider <north.tong@163.com>
2021-12-05 15:31:49 +0800

    Update TimerTest.java

--- src/main/java/com/northsunstrider/timer/TimerTest.java
+++ src/main/java/com/northsunstrider/timer/TimerTest.java
@@ -11,66 +11,66 @@ import java.util.TimerTask;
  */
 public class TimerTest {
 
-	/**
-	 * @Description TODO
-	 * @param args
-	 */
-	public static void main(String[] args) {
-		// timer1();
-		//
-		// System.out.println("===========");
-		//
-		// timer2();
-		// System.out.println("===========");
-		//
-		// timer3();
-		timer4();
-	}
+    /**
+     * @Description TODO
+     * @param args
+     */
+    public static void main(String[] args) {
+        // timer1();
+        //
+        // System.out.println("===========");
+        //
+        timer2();
+        // System.out.println("===========");
+        //
+        // timer3();
+        // timer4();
+    }
 
-	public static void timer1() {
-		Timer timer = new Timer();
-		timer.schedule(new TimerTask() {
+    public static void timer1() {
+        Timer timer = new Timer();
+        timer.schedule(new TimerTask() {
 
-			@Override
-			public void run() {
-				System.out.println(" timer1--timerTask");
-			}
-		}, 5000);
-	}
+            @Override
+            public void run() {
+                System.out.println(" timer1--timerTask");
+            }
+        }, 5000);
+    }
 
-	public static void timer2() {
-		Timer timer = new Timer();
-		timer.schedule(new TimerTask() {
+    public static void timer2() {
+        Timer timer = new Timer();
+        timer.schedule(new TimerTask() {
 
-			@Override
-			public void run() {
-				System.out.println(" timer2--timerTask");
-			}
-		}, new Date(System.currentTimeMillis() + 1000));
+            @Override
+            public void run() {
+                System.out.println(" timer2--timerTask");
+            }
+        }, new Date(System.currentTimeMillis() + 1000));
 
-	}
+    }
 
-	public static void timer3() {
-		Timer timer = new Timer();
-		timer.schedule(new TimerTask() {
+    public static void timer3() {
+        Timer timer = new Timer();
+        timer.schedule(new TimerTask() {
 
-			@Override
-			public void run() {
-				System.out.println(" timer3--timerTask");
-			}
-		}, new Date(System.currentTimeMillis()), 2000);
+            @Override
+            public void run() {
+                System.out.println(" timer3--timerTask");
+            }
+        }, new Date(System.currentTimeMillis()), 2000);
 
-	}
+    }
 
-	public static void timer4() {
-		Timer timer = new Timer();
-		timer.schedule(new TimerTask() {
+    public static void timer4() {
+        Timer timer = new Timer();
+        timer.schedule(new TimerTask() {
 
-			@Override
-			public void run() {
-				System.out.println(" timer4--timerTask");
-			}
-		}, 10000, 1000);
+            @Override
+            public void run() {
+                System.out.println(" timer4--timerTask");
+            }
+        }, 10000, 1000);
 
-	}
+    }
 }
