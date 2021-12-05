ee5c3e1fbd40bb643b6ee9b7b2828157456ccb8f
NorthSunstrider <north.tong@163.com>
2021-12-05 15:30:57 +0800

    Update StringTest.java

--- src/main/java/com/northsunstrider/string/StringTest.java
+++ src/main/java/com/northsunstrider/string/StringTest.java
@@ -7,48 +7,54 @@ package com.northsunstrider.string;
  */
 public class StringTest {
 
-	/**
-	 * @Description main方法
-	 * @param args
-	 */
-	public static void main(String[] args) {
-		compareStringTest();
-		stringPlusStringBuilderTest();
-	}
+    /**
+     * @Description main方法
+     * @param args
+     */
+    public static void main(String[] args) {
+        compareStringTest();
+        stringPlusStringBuilderTest();
+        int a;
+        System.out.println(a = 7);
 
-	/**
-	 * @Description 用==符号比较值相同的String对象
-	 */
-	public static void compareStringTest() {
-		String s1 = "abc";
-		String s2 = "a" + new String("bc");
+        String c = "abc";
+        String b = "a" + new String("bc");
+        System.out.println(c == b);
+    }
 
-		// 运行结果为false
-		System.out.println(s1 == s2);
-	}
+    /**
+     * @Description 用==符号比较值相同的String对象
+     */
+    public static void compareStringTest() {
+        String s1 = "abc";
+        String s2 = "a" + new String("bc");
 
-	/**
-	 * @Description 测试String和StringBuilder是否可以使用符号'+'
-	 */
-	public static void stringPlusStringBuilderTest() {
-		String s = "1bc";
-		StringBuilder sb = new StringBuilder("aaa");
-		// 编译可以通过，String和StringBuilder可以使用符号'+'
-		System.out.println(s + sb);
-	}
+        // 运行结果为false
+        System.out.println(s1 == s2);
+    }
 
-	/**
-	 * @Description 测试Java中的三个String对象
-	 */
-	public static void threeStringObjectTest() {
-		String str = new String("aaa");
-		StringBuffer sb = new StringBuffer("sbsb");
-		StringBuilder stringBuilder = new StringBuilder("builder");
+    /**
+     * @Description 测试String和StringBuilder是否可以使用符号'+'
+     */
+    public static void stringPlusStringBuilderTest() {
+        String s = "1bc";
+        StringBuilder sb = new StringBuilder("aaa");
+        // 编译可以通过，String和StringBuilder可以使用符号'+'
+        System.out.println(s + sb);
+    }
 
-		char a = 'a';
-		// 报错
-		// char b="a";
-		char c='他';
-	}
+    /**
+     * @Description 测试Java中的三个String对象
+     */
+    public static void threeStringObjectTest() {
+        String str = new String("aaa");
+        StringBuffer sb = new StringBuffer("sbsb");
+        StringBuilder stringBuilder = new StringBuilder("builder");
+
+        char a = 'a';
+        // 报错
+        // char b="a";
+        char c = '他';
+    }
 
 }
