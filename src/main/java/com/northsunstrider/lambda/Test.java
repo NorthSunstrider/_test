d34e6a24090776d6c8eb68ab99c409e3d354c444
NorthSunstrider <north.tong@163.com>
2022-08-23 09:43:56 +0800

    Update Test.java

--- src/main/java/com/northsunstrider/lambda/Test.java
+++ src/main/java/com/northsunstrider/lambda/Test.java
@@ -11,54 +11,59 @@ import java.util.function.Consumer;
  */
 public class Test {
 
-	/**
-	 * @Description main
-	 * @param args
-	 */
-	public static void main(String[] args) {
-		arraysLoopOriginal();
+    /**
+     * @Description main
+     * @param args
+     */
+    public static void main(String[] args) {
 
-		arraysLoopByLambda();
+        Arrays.asList(0, 1, 2, 3, 4, 5, 6, 7, 8, 10).stream().reduce(0, Integer::sum);
 
-		arraysLoopByLambda1();
+        Arrays.asList("a", "b", "d").forEach(System.out::println);
 
-		arraysLoop();
-	}
+        arraysLoopOriginal();
 
-	/**
-	 * @Description 传统方式的for循环
-	 */
-	public static void arraysLoopOriginal() {
-		for (String s : Arrays.asList("a", "b", "d"))
-			System.out.println(s);
-	}
+        arraysLoopByLambda();
 
-	/**
-	 * @Description java8新加lambda表达式实现的for循环
-	 */
-	public static void arraysLoopByLambda() {
-		Arrays.asList("a", "b", "d").forEach(e -> System.out.println(e));
-	}
+        arraysLoopByLambda1();
 
-	/**
-	 * @Description lambda表达式的简化版本双冒号表达式
-	 */
-	public static void arraysLoopByLambda1() {
-		String[] a = { "17", "52", "33", "412", "59", "62", "71" };
-		List<String> list = Arrays.asList(a);
-		// 这个是使用lambda表达式的简化版本双冒号表达式（调用out对象的println方法）
-		list.forEach(System.out::println);
-	}
+        arraysLoop();
+    }
 
-	/**
-	 * @Description java8新加的函数list.forEach()
-	 */
-	public static void arraysLoop() {
-		Arrays.asList("a", "b", "d").forEach(new Consumer<String>() {
-			public void accept(String t) {
-				System.out.println(t);
-			};
-		});
-	}
+    /**
+     * @Description 传统方式的for循环
+     */
+    public static void arraysLoopOriginal() {
+        for (String s : Arrays.asList("a", "b", "d"))
+            System.out.println(s);
+    }
+
+    /**
+     * @Description java8新加lambda表达式实现的for循环
+     */
+    public static void arraysLoopByLambda() {
+        Arrays.asList("a", "b", "d").forEach(e -> System.out.println(e));
+    }
+
+    /**
+     * @Description lambda表达式的简化版本双冒号表达式
+     */
+    public static void arraysLoopByLambda1() {
+        String[] a = {"17", "52", "33", "412", "59", "62", "71"};
+        List<String> list = Arrays.asList(a);
+        // 这个是使用lambda表达式的简化版本双冒号表达式（调用out对象的println方法）
+        list.forEach(System.out::println);
+    }
+
+    /**
+     * @Description java8新加的函数list.forEach()
+     */
+    public static void arraysLoop() {
+        Arrays.asList("a", "b", "d").forEach(new Consumer<String>() {
+            public void accept(String t) {
+                System.out.println(t);
+            };
+        });
+    }
 
 }
