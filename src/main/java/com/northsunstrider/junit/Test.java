48f405d7c81d90f86f300ca9ad7850be812b570c
NorthSunstrider <north.tong@163.com>
2024-10-17 21:44:08 +0800

    Changes

---
+++ src/main/java/com/northsunstrider/junit/Test.java
@@ -0,0 +1,10 @@
+package com.northsunstrider.junit;
+
+import cn.hutool.core.lang.Assert;
+
+public class Test {
+    public static void main(String[] args) {
+        String str="hello";
+        Assert.isTrue(str.length() > 0,  "");
+    }
+}
